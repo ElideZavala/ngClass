3a753d4e60077761fc079e2205d893cfce7f6600
ElideZavala <zavalavinagreelide@gmail.com>
2022-12-18 20:45:19 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dirNgClass';
+  fondoAmarillo = false;
+  letraGrande = false;
+
+  checkFondo() {
+    this.fondoAmarillo = !this.fondoAmarillo;
+  }
+
+  checkLetra() {
+    this.letraGrande = !this.letraGrande;
+  }
+
+  asignaClases() {
+    let classes = {
+      fa: this.fondoAmarillo,
+      lg: this.letraGrande
+    };
+
+    return classes;
+  }
 }
